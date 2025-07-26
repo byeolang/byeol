5e3c1c23f9aafc1ea29ee3980a905da37438dfc3
kniz <kniz@byeol.io>
2025-07-26 19:58:25 +0900

    byeol Theme와 byeol language syntax 추가
    주의: 별도로 repository를 만들 것.

--- play/monaco-config.mjs
+++ play/monaco-config.mjs
@@ -1,8 +1,104 @@
 import * as monaco from 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/+esm';
 
+monaco.languages.register({id: 'byeol'});
+monaco.languages.setMonarchTokensProvider('byeol', {
+  keywords: [
+    'def', 'as', 'is', 'ctor', 'on', 'in', 'next', 'else',
+    'for', 'if', 'ret', 'while', 'with', 'pack', 'break', 'get',
+    'set', 'end', 'true', 'false'
+  ],
+
+  typeKeywords: [
+    'void', 'err', 'int', 'super', 'byte', 'flt', 'str', 'char', 'me', 'it'
+  ],
+
+  operators: [
+    '=', '>', '<', '!', '~', ':', '==', '<=', '>=', '!=',
+    '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^', '%',
+    '<<', '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=', '^=',
+    '%=', ':='
+  ],
+
+  // we include these common regular expressions
+  symbols:  /[=><!~?:&|+\-*\/\^%\:]+/,
+
+  // C# style strings
+  escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
+  // The main tokenizer for our languages
+  tokenizer: {
+    root: [
+      // identifiers and keywords
+      [/[a-z_$][\w$]*/, { cases: { '@typeKeywords': 'type',
+                                   '@keywords': 'keyword',
+                                   '@default': 'identifier' } }],
+      [/[A-Z][\w\$]*/, 'type.identifier' ],  // to show class names nicely
+
+      // whitespace
+      { include: '@whitespace' },
+
+      // delimiters and operators
+      [/[{}()\[\]]/, '@brackets'],
+      [/[<>](?!@symbols)/, '@brackets'],
+      [/@symbols/, { cases: { '@operators': 'operator',
+                              '@default'  : '' } } ],
+
+      // numbers
+      [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+      [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+      [/\d+/, 'number'],
+
+      // delimiter: after number because of .\d floats
+      [/[,.]/, 'delimiter'],
+
+      // strings
+      [/"([^"\\]|\\.)*$/, 'string.invalid' ],  // non-teminated string
+      [/"/,  { token: 'string.quote', bracket: '@open', next: '@string' } ],
+    ],
+
+    comment: [
+    ],
+
+    string: [
+      [/[^\\"]+/,  'string'],
+      [/@escapes/, 'string.escape'],
+      [/\\./,      'string.escape.invalid'],
+      [/"/,        { token: 'string.quote', bracket: '@close', next: '@pop' } ]
+    ],
+
+    whitespace: [
+      [/[ \t\r\n]+/, 'white'],
+      [/#[^\n#]+/, 'comment'], // one line comment
+      [/##[^#]*##/, 'comment'], // multi line comment
+    ],
+  },
+});
+
+monaco.editor.defineTheme('byeolTheme', {
+  base: 'vs-dark',
+  inherit: false,
+  rules: [
+    { token: "keyword", foreground: "#8cc540" },
+    { token: "comment", foreground: "#556d69" },
+    { token: "number", foreground: "#b9a999" },
+    { token: "string", foreground: "#ff507f" },
+    { token: "delimiter", foreground: "#355689" },
+    { token: "operator", foreground: "#355689" },
+    { token: "identifier", foreground: "#b9a999" },
+    { token: "identifier.function", foreground: "#2d8bff" },
+    { token: "type", foreground: "#369d44" },
+
+  ],
+  colors: {
+    'editor.background': '#152330'
+  }
+});
+
 export const editor = monaco.editor.create(document.getElementById('codepad'), {
-  language: 'javascript',
-  theme: 'vs-dark',
-  automaticLayout: true
+  language: 'byeol',
+  theme: 'byeolTheme',
+  automaticLayout: true,
+  fontFamily: 'Retro Gaming',
+  fontSize: '20',
 });
 export const model = editor.getModel();
