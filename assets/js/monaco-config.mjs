b66fc5545320eb12462375930d1f0485638174bd
kniz <kniz@byeol.io>
2025-08-04 22:55:05 +0900

    refactor: move monaco editor byeol syntax to another repository

--- assets/js/monaco-config.mjs
+++ assets/js/monaco-config.mjs
@@ -1,79 +1,9 @@
 import * as monaco from 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/+esm';
 import { DoxygenAwesomeDarkModeToggle } from './doxygen-awesome-darkmode-toggle.mjs';
+import { byeolLanguage } from './monaco-editor-byeol-language-min.mjs';
 
 monaco.languages.register({id: 'byeol'});
-monaco.languages.setMonarchTokensProvider('byeol', {
-  keywords: [
-    'def', 'as', 'is', 'ctor', 'on', 'in', 'next', 'else',
-    'for', 'if', 'ret', 'while', 'with', 'pack', 'break', 'get',
-    'set', 'end', 'true', 'false'
-  ],
-
-  typeKeywords: [
-    'void', 'err', 'int', 'super', 'byte', 'flt', 'str', 'char', 'me', 'it'
-  ],
-
-  operators: [
-    '=', '>', '<', '!', '~', ':', '==', '<=', '>=', '!=',
-    '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^', '%',
-    '<<', '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=', '^=',
-    '%=', ':='
-  ],
-
-  // we include these common regular expressions
-  symbols:  /[=><!~?:&|+\-*\/\^%\:]+/,
-
-  // C# style strings
-  escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
-
-  // The main tokenizer for our languages
-  tokenizer: {
-    root: [
-      // identifiers and keywords
-      [/[a-z_$][\w$]*/, { cases: { '@typeKeywords': 'type',
-                                   '@keywords': 'keyword',
-                                   '@default': 'identifier' } }],
-      [/[A-Z][\w\$]*/, 'type.identifier' ],  // to show class names nicely
-
-      // whitespace
-      { include: '@whitespace' },
-
-      // delimiters and operators
-      [/[{}()\[\]]/, '@brackets'],
-      [/[<>](?!@symbols)/, '@brackets'],
-      [/@symbols/, { cases: { '@operators': 'operator',
-                              '@default'  : '' } } ],
-
-      // numbers
-      [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
-      [/0[xX][0-9a-fA-F]+/, 'number.hex'],
-      [/\d+/, 'number'],
-
-      // delimiter: after number because of .\d floats
-      [/[,.]/, 'delimiter'],
-
-      // strings
-      [/"([^"\\]|\\.)*$/, 'string.invalid' ],  // non-teminated string
-      [/"/,  { token: 'string.quote', bracket: '@open', next: '@string' } ],
-    ],
-
-    comment: [
-    ],
-
-    string: [
-      [/[^\\"]+/,  'string'],
-      [/@escapes/, 'string.escape'],
-      [/\\./,      'string.escape.invalid'],
-      [/"/,        { token: 'string.quote', bracket: '@close', next: '@pop' } ]
-    ],
-
-    whitespace: [
-      [/[ \t\r\n]+/, 'white'],
-      [/#[^\n#]+/, 'comment'], // one line comment
-      [/##[^#]*##/, 'comment'], // multi line comment
-    ],
-  },
-});
+monaco.languages.setMonarchTokensProvider('byeol', byeolLanguage);
 
 const colors = {
   white: "#FFFFFF",
