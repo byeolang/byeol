b5ec0b5d64548cc9f2b5262e0b8e8918985bdce8
kniz <kniz@byeol.io>
2025-08-11 23:16:39 +0900

    frontend: style(fileFlag): change file extention to `byeol`

--- module/frontend/flag/flags/fileFlag.cpp
+++ module/frontend/flag/flags/fileFlag.cpp
@@ -6,17 +6,19 @@
 namespace by {
     BY_DEF_ME(fileFlag)
 
+#define FILE_EXTENSION "byeol"
+
     const nchar* me::getName() const { return "<filePath>"; }
 
     const nchar* me::getDescription() const {
         return R"DESC(
     file path of frontend language script file.
-    extension of file should be end with 'nm'.
+    extension of file should be end with ')DESC" FILE_EXTENSION R"DESC('.
     wildcard and the question mark aren't allowed.)DESC";
     }
 
     const strings& me::_getRegExpr() const {
-        static strings inner{"[\\\\\\w\\.\\/:\\-\\(\\)\\d]+\\.nm$"};
+        static strings inner{"[\\\\\\w\\.\\/:\\-\\(\\)\\d]+\\." FILE_EXTENSION "$"};
         return inner;
     }
 
