730e120c58a97a1f4669c7dfb550c81d74280d39
kniz <kniz@byeol.io>
2025-08-23 10:43:11 +0900

    style: apply formatter

--- module/indep/helper/tmedium.hpp
+++ module/indep/helper/tmedium.hpp
@@ -17,33 +17,33 @@ namespace by {
     /// and return it in the middle, even if the rhs entered into the OR macro is nullptr.
     /// sometimes, there are cases where you need to immediately wrap the T& returned by the OR
     /// macro with another container or wrapper class that can receive it. for example,
-    /// 
+    ///
     /// @code
     /// A& got = makeAOnHeap() OR.ret();
     /// // since this `got` variable is on the heap, we will use tstr to easily manage
     /// memory. tstr<A> gotLife(got);
     /// @endcode
-    /// 
+    ///
     /// if you don't want to create two variables like above, you may want to do this:
-    /// 
+    ///
     /// @code
     /// str wrap = getA() OR.ret();
     /// @endcode
-    /// 
+    ///
     /// but, there is one problem in this way.
     /// the c++ standard does not specify that the address value of a null reference is null.
     /// (and in fact, in an environment where optimized code is run by clang on silicon macOS,
     /// referencing the address value of a nulled reference returns a garbage value.)
     /// so, when a null reference is received in the constructor of the tstr class, there is no
     /// way for the tstr class to determine whether it is correct or not.
-    /// 
+    ///
     /// then you might be thinking, `Why not just make the OR macro return a T*?`.
     /// if it returns T*, the meaning of using OR macro is greatly diminished.
     /// because users can no longer change from unsafe type to safe type easily, they might need
     /// to use some sort of c-style casting.
-    /// 
+    ///
     /// that's where `tmedium` class comes in. `tmedium` has the following purposes:
-    /// 
+    ///
     /// 1. it should be very lightweight.
     /// 2. it should have T* inside, but act like T& outside.
     /// 3. it should be able to determine whether it is a valid T& or not from outside
